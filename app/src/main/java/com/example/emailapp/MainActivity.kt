5bb9d36bf9575c49ac35e0a750ce808bba044cac
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-20 20:46:58 +0200

    button send email

--- app/src/main/java/com/example/emailapp/MainActivity.kt
+++ app/src/main/java/com/example/emailapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.emailapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        val sendButton = findViewById<Button>(R.id.ButtonSend)
+        sendButton.setOnClickListener {
+
+            val email = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.email).text.toString()
+            val subject = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.subject).text.toString()
+            val message = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.message).text.toString()
+            val emails = arrayOf(email)
+
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, emails)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            startActivity(intent)
+    }}
 }
